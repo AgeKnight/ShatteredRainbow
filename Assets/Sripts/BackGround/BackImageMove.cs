a4862fb086edf6d06f737e0b63e44f547012404f
owenbai334 <owenbai91@gmail.com>
2024-03-21 21:40:08 +0800

    114514

---
+++ Assets/Sripts/BackGround/BackImageMove.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackImageMove : MonoBehaviour
+{
+    bool isChanged = false ;
+    RectTransform rt;
+    public float speed;
+    public GameObject BackImages;
+    public float[] ImageTransform;
+    public Sprite[] Images;
+    void Awake() {
+        rt = BackImages.GetComponent<RectTransform>();
+    }
+    void Update()
+    {
+        Move();
+    }
+    void Move()
+    {
+        rt.transform.Translate(0,-1*speed*Time.deltaTime,0);
+        if(rt.anchoredPosition3D.y<=ImageTransform[0]&&!isChanged)
+        {
+            isChanged=true;
+            InfinityMove();
+        }
+    }
+    void InfinityMove()
+    {
+        GameObject temp = BackImages.transform.GetChild(0).gameObject;
+        if(GameManager.Instance.enemyManager.isSpanBoss)
+        {
+            BackImages.GetComponent<Image>().sprite = Images[2];
+        }
+        else
+        {
+            BackImages.GetComponent<Image>().sprite = Images[1];
+        }
+        rt.transform.GetChild(0).SetParent(rt.transform.parent);
+        rt.anchoredPosition3D = new Vector3 (0, ImageTransform[1], 0);
+        rt.SetParent(temp.transform);
+        BackImages = temp;
+        rt = BackImages.GetComponent<RectTransform>();
+        isChanged=false;
+    }
+}
